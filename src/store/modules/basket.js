e08454f0e061bcf3483138933d41263b0614dcd1
pukochka <tema.puko@gmail.com>
2022-06-07 22:08:53 +0300

    final validate

--- src/store/modules/basket.js
+++ src/store/modules/basket.js
@@ -8,7 +8,7 @@ export default {
     getUserData() {
       axios
         .post(
-          `api.bot-t.ru/v1/bot/user/view-by-telegram-id?secretKey=db0b766fdbc2274841d28673d0f4cf15dc311b9827f7c7cb2539d05a0f1c317e`,
+          `https://api.bot-t.ru/v1/bot/user/view-by-telegram-id?secretKey=db0b766fdbc2274841d28673d0f4cf15dc311b9827f7c7cb2539d05a0f1c317e`,
           {
             bot_id: 12845,
             telegram_id: 1028741753,
