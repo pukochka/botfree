30bbca982fdc4c1fcc14fed912af16b938c6b0f6
pukochka <tema.puko@gmail.com>
2022-06-15 19:47:50 +0300

    test auth

--- src/pages/userPage.vue
+++ src/pages/userPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="bg-white">
     <div class="q-pa-lg flex center max-xxl text-h4">Каталог товаров бота</div>
-    <div class="max-sm">{{ convertURL(getInitData.data.user) }}</div>
+    <div class="max-xs center">{{ getInitData.data }}</div>
     <div class="flex center max-xxl q-gutter-md">
       <q-btn
         rounded
